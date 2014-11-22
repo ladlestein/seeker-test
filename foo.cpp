70e51ff07b6f3b17a1d5467aa7a2219d63fdb7ae
Larry Edelstein <ladlestein@gmail.com>
2014-11-22 13:29:02 -0800

    fixes bad change

--- foo.cpp
+++ foo.cpp
@@ -4,7 +4,7 @@ int main(int argc, char *argv[]) {
   printf("hello world!\n");
   printf("something innocuous.\n");
 
-  int foo;
+  int foo = 1;
   printf("there is a foo with #: %d\n", foo);
   return 0;
 }
